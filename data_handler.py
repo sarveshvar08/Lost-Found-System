54d9fdd3e4dfb931e3d6369c460caafa7493d28c
sarveshvar08 <sarveshvar08@gmail.com>
2026-04-03 08:18:57 +0530

    Data handler created

--- data_handler.py
+++ data_handler.py
@@ -3,11 +3,9 @@ import os
 
 FILE_NAME = "data_files.csv"
 
-
+# Save item data to CSV
 def save_item(item):
-    """
-    Save item to CSV file
-    """
+    
     file_exists = os.path.isfile(FILE_NAME)
 
     with open(FILE_NAME, mode='a', newline='', encoding='utf-8') as file:
@@ -20,11 +18,9 @@ def save_item(item):
 
         writer.writerow(item)
 
-
+# Read items from CSV and return as a list of dictionaries
 def read_items():
-    """
-    Read all items from CSV
-    """
+
     items = []
 
     if not os.path.exists(FILE_NAME):
@@ -42,11 +38,9 @@ def read_items():
 
     return items
 
-
+# Search items by keyword in name or description
 def search_items(keyword):
-    """
-    Search items by keyword
-    """
+   
     results = []
     items = read_items()
 
